52d9d2a4a53ead237ac92aa8f665b2f9fba35c79
tu4boi <tuanattawut@gmail.com>
2021-01-18 18:31:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ListViews',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListViews')),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+Widget _myListView(BuildContext context) {
+  final titles = [
+    'bike',
+    'boat',
+    'bus',
+    'car',
+    'railway',
+    'run',
+    'subway',
+    'transit',
+    'walk',
+    'directions'
+  ];
+
+  final icons = [
+    Icons.directions_bike,
+    Icons.directions_boat,
+    Icons.directions_bus,
+    Icons.directions_car,
+    Icons.directions_railway,
+    Icons.directions_run,
+    Icons.directions_subway,
+    Icons.directions_transit,
+    Icons.directions_walk,
+    Icons.directions
+  ];
+
+  return ListView.builder(
+    itemCount: titles.length,
+    itemBuilder: (context, index) {
+      return Card(
+        //                           <-- Card widget
+        child: ListTile(
+          leading: Icon(icons[index]),
+          title: Text(titles[index]),
+        ),
+      );
+    },
+  );
+}
